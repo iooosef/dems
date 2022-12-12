af39c966c60cf18926d587951c3f31e300003175
iooosef <josence22@gmail.com>
2022-12-12 15:09:33 +0800

    Removed .\webapp directory and contents

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
 
 const IS_DEV = !process.env.NODE_ENV === 'production'
 module.exports = {
-    // 是否生成map文件
     productionSourceMap: IS_DEV ? true : true,
     outputDir: "./web"
 }
