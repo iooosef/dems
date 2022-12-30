7405698e934d7813d415a501d913309a86405d20
iooosef <josence22@gmail.com>
2022-12-30 22:05:49 +0800

    Added major layout
    Added layout consisting of navigation sidebar, header, and main body including basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <div class="container-fluid">
+    <div class="row vh-100">
+      <nav class="col-2 bg-success">
+        sidebar
+      </nav>
+      <div class="col-10 p-0">
+        <header class="w-100 border bg-primary">
+          test
+        </header>
+        <main class="w-100 bg-warning">
+          main
+        </main>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,5 +27,11 @@ export default {
 </script>
 
 <style scoped>
+header {
+  height: 64px;
+}
 
+main {
+  height: calc(100% - 64px);
+}
 </style>
