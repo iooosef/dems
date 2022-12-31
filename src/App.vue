4f39909ef0446d0e6ea393f426789e56e3898b34
iooosef <josence22@gmail.com>
2022-12-31 09:56:38 +0800

    Added mainstyles.scss and globally import it
    Solution is based on the source:
    https://austingil.com/global-sass-vue-project/

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/assets/styles/variables.scss";
+
 nav {
   box-shadow: $box-shadow-dems;
 }
--- vue.config.js
+++ vue.config.js
@@ -18,4 +18,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/styles/mainstyles.scss";`,
+      },
+    },
+  }
+}
 
