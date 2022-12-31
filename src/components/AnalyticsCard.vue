76daee03f378cdb2df23c33fcb79a053cc02c35c
iooosef <josence22@gmail.com>
2022-12-31 17:49:08 +0800

    Added the three analytics widgets

--- src/components/AnalyticsCard.vue
+++ src/components/AnalyticsCard.vue
@@ -1,6 +1,32 @@
 <template>
-    <section>
-        TEST
+    <section class="d-flex flex-row justify-content-between">
+        <div class="d-flex flex-fill border border-warning rounded-4">
+            <div class="p-3 flex-grow-1">
+                <h4>Evacuees</h4>
+                <h1 class="analytics-data-num">{{ analytics }}</h1>
+            </div>
+            <div class="p-3 flex-shrink-1">
+                <img src="@/assets/img/analytics_evacuees.png" class="analytics-img" alt="">
+            </div>
+        </div>
+        <div class="mx-4 d-flex flex-fill border border-warning rounded-4">
+            <div class="p-3 flex-grow-1">
+                <h4>Evacuees</h4>
+                <h1 class="analytics-data-num">{{ analytics }}</h1>
+            </div>
+            <div class="p-3 flex-shrink-1">
+                <img src="@/assets/img/analytics_family.png" class="analytics-img" alt="">
+            </div>
+        </div>
+        <div class="d-flex flex-fill border border-warning rounded-4">
+            <div class="p-3 flex-grow-1">
+                <h4>Evacuees</h4>
+                <h1 class="analytics-data-num">{{ analytics }}</h1>
+            </div>
+            <div class="p-3 flex-shrink-1">
+                <img src="@/assets/img/analytics_aid.png" class="analytics-img" alt="">
+            </div>
+        </div>
     </section>
 </template>
 
@@ -8,13 +34,21 @@
 export default {
     name: "AnalyticsCard",
     setup () {
-        
+        let analytics = 231;
 
-        return {}
+        return {analytics}
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.analytics-data-num {
+    font-size: 2.8rem;
+}
 
+.analytics-img {
+    width: 80%;
+    height: auto;
+    float: right;
+}
 </style>
