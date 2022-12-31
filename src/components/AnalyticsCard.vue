9914e8bd7d0660d0842c65839452f153cb3e3974
iooosef <josence22@gmail.com>
2022-12-31 18:28:04 +0800

    Changed flex-fill of each widget by w-100

--- src/components/AnalyticsCard.vue
+++ src/components/AnalyticsCard.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="d-flex flex-row justify-content-between">
-        <div class="d-flex flex-fill bg-dems-secondary shadow-inset rounded-4">
+        <div class="w-100 d-flex bg-dems-secondary shadow-inset rounded-4">
             <div class="p-3 flex-grow-1">
                 <h4>Evacuees</h4>
                 <h1 class="analytics-data-num">{{ analytics }}</h1>
@@ -9,18 +9,18 @@
                 <img src="@/assets/img/analytics_evacuees.png" class="analytics-img" alt="">
             </div>
         </div>
-        <div class="mx-4 d-flex flex-fill bg-dems-secondary shadow-inset rounded-4">
+        <div class="w-100 mx-4 d-flex bg-dems-secondary shadow-inset rounded-4">
             <div class="p-3 flex-grow-1">
-                <h4>Evacuees</h4>
+                <h4>Families</h4>
                 <h1 class="analytics-data-num">{{ analytics }}</h1>
             </div>
             <div class="p-3 flex-shrink-1">
                 <img src="@/assets/img/analytics_family.png" class="analytics-img" alt="">
             </div>
         </div>
-        <div class="d-flex flex-fill bg-dems-secondary shadow-inset rounded-4">
-            <div class="p-3 flex-grow-1">
-                <h4>Evacuees</h4>
+        <div class="w-100 d-flex bg-dems-secondary shadow-inset rounded-4">
+            <div class="ps-3 py-3 flex-grow-1">
+                <h4>Needs Medical Assistance</h4>
                 <h1 class="analytics-data-num">{{ analytics }}</h1>
             </div>
             <div class="p-3 flex-shrink-1">
