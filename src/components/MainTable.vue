0ffdbef1f8ed12eaafa87ec727e96b2faacd34b6
iooosef <josence22@gmail.com>
2023-01-01 20:03:37 +0800

    Styled search bar and added search icon

--- src/components/MainTable.vue
+++ src/components/MainTable.vue
@@ -2,18 +2,24 @@
     <div class="mt-4 flex-grow-1">
         <div class="p-3 table-header d-flex justify-content-between bg-dems-light rounded-3">
             <h4>{{ tableLabel }}</h4>
-            <div class="search-box bg-dems-light shadow-inset">
-                test
+            <div class="search-input-group">
+                <Icon icon="dems:search" height="1rem" class="search-icon" />
+                <input class="h-100 search-box form-control bg-dems-white shadow-inset" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue'
+import "@/assets/icons"
 import { ref } from 'vue';
 
 export default {
     name: "MainTable",
+    components: {
+        Icon
+    },
     setup () {
         const tableLabel = ref("Table Name")
 
@@ -27,7 +33,19 @@ export default {
     height: 60px;
 }
 
-.search-box {
+.search-input-group {
+    position: relative;
+}
+.search-input-group > input {
     width: 200px;
+    padding-left: 25px;
+}
+
+.search-icon {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: .5rem;
+    margin: auto 0;
 }
 </style>
