520a19e8710f6a3d84e5a6f4d1fd32f7cf233a7d
iooosef <josence22@gmail.com>
2023-01-01 17:58:42 +0800

    added table header and components

--- src/components/MainTable.vue
+++ src/components/MainTable.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="mt-4 flex-grow-1 bg-dems-light">
-        <h1>TABLE</h1>
+    <div class="mt-4 flex-grow-1">
+        <div class="p-3 table-header d-flex justify-content-between bg-dems-light rounded-3">
+            <h4>{{ tableLabel }}</h4>
+            <div class="search-box bg-dems-light shadow-inset">
+                test
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
     name: "MainTable",
     setup () {
-        
+        const tableLabel = ref("Table Name")
 
-        return {}
+        return {tableLabel}
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.table-header {
+    height: 60px;
+}
 
+.search-box {
+    width: 200px;
+}
 </style>
