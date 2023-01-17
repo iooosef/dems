51c4e5f5ac1e7ad8f58a62cc2af5b98f049a0005
iooosef <josence22@gmail.com>
2023-01-17 23:03:56 +0800

    Added button template for navigation sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="flex flex-row h-screen bg-white">
-        <nav class="w-60 p-5 bg-white drop-shadow-border z-20">test</nav>
+        <nav class="w-60 p-5 bg-white drop-shadow-border z-20">
+            <img src="@/assets/img/logo.png" alt="Disaster Evacuees Management System logo" class="w-full" />
+            <div class="w-full mt-4 p-3 bg-primary rounded-lg text-2xl text-white font-semibold">
+                New Entry
+            </div>
+            <div class="w-full mt-4 p-3 bg-light rounded-lg text-xl text-black font-semibold">
+                Evacuees
+            </div>
+        </nav>
         <div class="flex flex-col flex-1 bg-white">
             <header class="h-16 p-5 bg-white drop-shadow-border z-10">header</header>
             <main class="flex-1 p-5 bg-white drop-shadow-border z-0">main</main>
