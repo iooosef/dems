ea015b791801677d4a2ed56a5b7ec69f0dfe6923
iooosef <josence22@gmail.com>
2023-01-17 22:31:21 +0800

    Added general page layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        test
+    <div class="flex flex-row h-screen bg-white">
+        <nav class="w-60 bg-white">test</nav>
+        <div class="flex flex-col flex-1 bg-white">
+            <header class="h-16 bg-white">header</header>
+            <main class="flex-1 bg-white">main</main>
+        </div>
     </div>
 </template>
 
