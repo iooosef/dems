6bdb3ea134a90237bd0b8669f7fe31bf9ea01846
iooosef <josence22@gmail.com>
2023-01-17 17:26:07 +0800

    Added color theme to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {}
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      primary: '#3498db',
+      secondary: '#77bce9',
+      dark: '#1c3144',
+      light: '#ecf0f1'
+    }
   },
   plugins: []
 }
