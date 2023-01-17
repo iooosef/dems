3e32c8cea0a70665eba2897564f9231227229824
iooosef <josence22@gmail.com>
2023-01-17 22:41:10 +0800

    Added drop shadow border style to general layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-row h-screen bg-white">
-        <nav class="w-60 bg-white">test</nav>
+        <nav class="w-60 bg-white drop-shadow-border z-20">test</nav>
         <div class="flex flex-col flex-1 bg-white">
-            <header class="h-16 bg-white">header</header>
-            <main class="flex-1 bg-white">main</main>
+            <header class="h-16 bg-white drop-shadow-border z-10">header</header>
+            <main class="flex-1 bg-white drop-shadow-border z-0">main</main>
         </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
       secondary: '#77bce9',
       dark: '#1c3144',
       light: '#ecf0f1'
+    },
+    extend: {
+      dropShadow: {
+        border: '0px 3px 8px rgba(0, 0, 0, 0.2)'
+      }
     }
   },
   plugins: []
