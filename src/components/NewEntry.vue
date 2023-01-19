b628a6e43b64d2cdaf920af76d64a868c59a27af
iooosef <josence22@gmail.com>
2023-01-19 10:20:34 +0800

    Added base layout to new entry menu buttons

--- src/components/NewEntry.vue
+++ src/components/NewEntry.vue
@@ -1,10 +1,28 @@
 <template>
   <PrimeDialog
-    v-model:visible="displayModalProp" modal="true" :closable="false"
-    :draggable="false"
+    v-model:visible="displayModalProp" modal="true"
+    :closable="true" :draggable="false"
     class="w-6/12 [&>*]:!bg-white [&>*:nth-child(2)]:!rounded-t-lg [&>*:nth-child(3)]:!rounded-b-lg"
   >
-    test
+    <template #header>
+      <h2 class="text-2xl font-bold">
+        Select a new entry
+      </h2>
+    </template>
+    <div class="mt-2 grid grid-cols-2 grid-rows-2 gap-3">
+      <button class="p-4 rounded-lg bg-primary-500 rounded-lg text-xl text-white font-medium hover:bg-primary-700 focus:shadow-inner focus:outline-none focus:ring focus:ring-offset-0 focus:ring-primary-500/50 transition duration-200 cursor-pointer">
+        test
+      </button>
+      <button class="p-4 rounded-lg bg-primary-500 rounded-lg text-xl text-white font-medium hover:bg-primary-700 focus:shadow-inner focus:outline-none focus:ring focus:ring-offset-0 focus:ring-primary-500/50 transition duration-200 cursor-pointer">
+        test
+      </button>
+      <button class="p-4 rounded-lg bg-primary-500 rounded-lg text-xl text-white font-medium hover:bg-primary-700 focus:shadow-inner focus:outline-none focus:ring focus:ring-offset-0 focus:ring-primary-500/50 transition duration-200 cursor-pointer">
+        test
+      </button>
+      <button class="p-4 rounded-lg bg-primary-500 rounded-lg text-xl text-white font-medium hover:bg-primary-700 focus:shadow-inner focus:outline-none focus:ring focus:ring-offset-0 focus:ring-primary-500/50 transition duration-200 cursor-pointer">
+        test
+      </button>
+    </div>
   </PrimeDialog>
 </template>
 
